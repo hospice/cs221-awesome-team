243e4199d9f2bfab76967b128abcba2f6d114fb7
pensadov@gmail.com <pensadov@gmail.com@9f389937-5a79-6ef3-705e-07bf4cbd8fe1>
2013-01-15 19:16:40 +0000

    Setup tests for PartA

---
+++ trunk/project1/Assignment 1/src/ir/assignments/two/tests/PartA.java
@@ -0,0 +1,19 @@
+package ir.assignments.two.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PartA {
+
+	@Test
+	public void testTokenizeFile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPrintFrequencies() {
+		fail("Not yet implemented");
+	}
+
+}
