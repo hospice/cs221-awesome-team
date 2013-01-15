06ab57d6c26cfd2cbff8274d52632a1bc97127f5
pensadov <pensadov@gmail.com>
2013-01-15 23:05:37 +0000

    Added test for PartB

---
+++ project1/Assignment 1/src/ir/assignments/two/tests/PartBTest.java
@@ -0,0 +1,43 @@
+package ir.assignments.two.tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+import ir.assignments.two.a.Frequency;
+import ir.assignments.two.b.WordFrequencyCounter;
+
+import org.junit.Test;
+
+public class PartBTest {
+
+	@Test
+	public void testComputeWordFrequencies() {
+		List<String> words = Arrays.asList(new String[] { "this", "sentence", "repeats", "the", "word", "sentence" });
+		List<Frequency> frequencies = WordFrequencyCounter.computeWordFrequencies(words);
+		ArrayList<Frequency> expected = new ArrayList<Frequency>();
+		expected.add(new Frequency("sentence", 2));
+		expected.add(new Frequency("the", 1));
+		expected.add(new Frequency("this", 1));
+		expected.add(new Frequency("repeats", 1));
+		expected.add(new Frequency("word", 1));
+		
+		// Can't use assertArrayEquals because we want to compare the objects (not the object references)
+		compareFrequencyLists(expected, frequencies);
+	}
+	
+	private void compareFrequencyLists(List<Frequency> expected, List<Frequency> actual) {
+		assertEquals(expected.size(), actual.size());
+		
+		for (int i = 0; i < expected.size(); i++)
+		{
+			Frequency expectedFreq = expected.get(i);
+			Frequency actualFreq = actual.get(i);
+			
+			assertEquals(expectedFreq.getText(), actualFreq.getText());
+			assertEquals(expectedFreq.getFrequency(), actualFreq.getFrequency());
+		}
+	}	
+}
