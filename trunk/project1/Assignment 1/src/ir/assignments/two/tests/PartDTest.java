9be22724a8ccd08e9521c57d02d595c0735b9066
pensadov@gmail.com <pensadov@gmail.com@9f389937-5a79-6ef3-705e-07bf4cbd8fe1>
2013-01-17 00:05:45 +0000

    Added more PartD tests, cleaned up PartD implementation a bit

--- trunk/project1/Assignment 1/src/ir/assignments/two/tests/PartDTest.java
+++ trunk/project1/Assignment 1/src/ir/assignments/two/tests/PartDTest.java
@@ -19,12 +19,47 @@ public class PartDTest {
 	public void testComputePalindromeFrequencies() {
 		ArrayList<String> words = new ArrayList<String>(Arrays.asList(new String[] { "do", "geese", "see", "god", "abba", "bat", "tab" }));
 		List<Frequency> actual = computePalindromeFrequencies(words);
-		
+
 		ArrayList<Frequency> expected = new ArrayList<Frequency>();
 		expected.add(new Frequency("do geese see god", 1));
 		expected.add(new Frequency("bat tab", 1));
 		expected.add(new Frequency("abba", 1));
-		
+
+		TestUtils.compareFrequencyLists(expected, actual);
+	}
+	
+	@Test
+	public void testComputePalindromeFrequencies_TiesOrderAlphabetical() {
+		ArrayList<String> words = new ArrayList<String>(Arrays.asList(new String[] { "abba", "abba", "cbbc", "cbbc" }));
+		List<Frequency> actual = computePalindromeFrequencies(words);
+
+		ArrayList<Frequency> expected = new ArrayList<Frequency>();
+		expected.add(new Frequency("abba abba", 1));
+		expected.add(new Frequency("cbbc cbbc", 1));
+		expected.add(new Frequency("abba", 2));
+		expected.add(new Frequency("cbbc", 2));
+
+		TestUtils.compareFrequencyLists(expected, actual);
+	}
+	
+	@Test
+	public void testComputePalindromeFrequencies_TiesOrderFrequency() {
+		ArrayList<String> words = new ArrayList<String>(Arrays.asList(new String[] { "kayak", "kayak", "aabaa" }));
+		List<Frequency> actual = computePalindromeFrequencies(words);
+
+		ArrayList<Frequency> expected = new ArrayList<Frequency>();
+		expected.add(new Frequency("kayak kayak", 1));
+		expected.add(new Frequency("kayak", 2));
+		expected.add(new Frequency("aabaa", 1));
+
+		TestUtils.compareFrequencyLists(expected, actual);
+	}
+
+	@Test
+	public void testComputePalindromeFrequencies_Null() {
+		List<Frequency> actual = computePalindromeFrequencies(null);
+		ArrayList<Frequency> expected = new ArrayList<Frequency>();
+
 		TestUtils.compareFrequencyLists(expected, actual);
 	}
 
@@ -34,19 +69,24 @@ public class PartDTest {
 			Class params[] = new Class[] { ArrayList.class };
 			Method method = PalindromeFrequencyCounter.class.getDeclaredMethod("computePalindromeFrequencies", params);
 			method.setAccessible(true);
-			return (List<Frequency>)method.invoke(null, words);
-		} catch (SecurityException e) {
-			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
+			return (List<Frequency>) method.invoke(null, words);
+		}
+		catch (SecurityException e) {
 			e.printStackTrace();
 		}
-		
+		catch (NoSuchMethodException e) {
+			e.printStackTrace();
+		}
+		catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		}
+		catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
+		catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+
 		return null;
 	}
 }
