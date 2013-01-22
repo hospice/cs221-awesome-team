43c412d84b84bb5d11a275d444e48140bb27b17a
pensadov <pensadov@gmail.com>
2013-01-22 20:09:34 +0000

    Copied over required classes from last project

--- project2/Assignment 2/src/ir/assignments/three/UrlStatistics.java
+++ project2/Assignment 2/src/ir/assignments/three/UrlStatistics.java
@@ -1,28 +1,34 @@
 package ir.assignments.three;
 
-import java.util.Collection;
+import ir.assignments.two.a.*;
 
-import com.sun.xml.internal.bind.v2.schemagen.xmlschema.List;
+import java.util.Collection;
+import java.util.List;
 
 public class UrlStatistics {
 	public static int countUniquePages(Collection<String> urls) {
 		// Count the number of unique pages in the URL list (e.g. http://www.example.com/test.php and http://www.example.com/test.php?param are 2 URLs but 1 page
+		return -1;
 	}
 	
 	public static List<Frequency> countSubdomains(Collection<String> urls) {
 		// http://vision.ics.uci.edu, 10
         // etc.
+		return null;
 	}
 	
 	public static String getLongestPage(Collection<String> urls) {
 		// Get the page with the most terms (including or excluding stop words?)
+		return null;
 	}
 	
 	public static List<String> getMostCommonWords(Collection<String> urls) {
 		// Get top 500 most common words across all documents (excluding stop words)
+		return null;
 	}
 	
 	public static List<String> getMostCommon2Grams(Collection<String> urls) {
 		// Get top 20 2-grams excluding stop words
+		return null;
 	}
 }
