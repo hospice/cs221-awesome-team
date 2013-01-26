0bb9b27bfc63248cf9cb0c14bfeccc1dd89b62ac
pensadov <pensadov@gmail.com>
2013-01-26 22:25:49 +0000

    Got crawler working (stays within the seed domain)

--- project2/Assignment 2/src/ir/assignments/three/ICSCrawler.java
+++ project2/Assignment 2/src/ir/assignments/three/ICSCrawler.java
@@ -1,5 +1,7 @@
 package ir.assignments.three;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.regex.Pattern;
 
 import edu.uci.ics.crawler4j.crawler.Page;
@@ -11,7 +13,7 @@ import edu.uci.ics.crawler4j.url.WebURL;
  * General code structure borrowed from: http://code.google.com/p/crawler4j/source/browse/src/test/java/edu/uci/ics/crawler4j/examples/basic/BasicCrawler.java
 */
 public class ICSCrawler extends WebCrawler {
-	private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g" + "|png|tiff?|mid|mp2|mp3|mp4" + "|wav|avi|mov|mpeg|ram|m4v|pdf" + "|rm|smil|wmv|swf|wma|zip|rar|gz))$");
+	private final static Pattern FILTERS = Pattern.compile(".*(\\.(css|js|bmp|gif|jpe?g|png|tiff?|mid|mp2|mp3|mp4|wav|avi|mov|mpeg|ram|m4v|pdf|rm|smil|wmv|swf|wma|zip|rar|gz|ico))$");
 
 	private ICSCrawlerStatistics stats = new ICSCrawlerStatistics();
 	private ICSCrawlerParameters params;
@@ -25,13 +27,25 @@ public class ICSCrawler extends WebCrawler {
 	@Override
 	public boolean shouldVisit(WebURL url) {
 		// Don't crawl non-HTML pages
-		String href = url.getURL().toLowerCase(); // Turns http://www.ics.uci.edu/somepage.php -> http://www.ics.uci.edu/
-		if (FILTERS.matcher(href).matches())
+		String href = url.getURL().toLowerCase(); // Turns http://www.ics.uci.edu/SomePage.PHP ->  http://www.ics.uci.edu/somepage.php
+		if (FILTERS.matcher(href).matches()) // filter using file extension
 			return false;
 		
 		// Only crawl within the domain of the seed URL
-		if (!url.getDomain().endsWith(this.params.getSeedUrl())) //TODO: this doesn't work
+		try {
+			URI currentUri = new URI(url.getURL());
+			String currentUrlHost = currentUri.getHost();
+			
+			URI seedUri = new URI(this.params.getSeedUrl());
+			String seedUrlHost = seedUri.getHost();
+			
+			if (!currentUrlHost.endsWith(seedUrlHost))
+				return false;
+		}
+		catch (URISyntaxException e) {
+			System.out.println("Error: " + e.getMessage());
 			return false;
+		}
 		
 		return true;
 	}
@@ -41,6 +55,7 @@ public class ICSCrawler extends WebCrawler {
 		// Keep track of visited URLs
 		String url = page.getWebURL().getURL();
 		stats.addCrawledPageUrl(url);
+		System.out.println("Crawled: " + url);
 
 		// Get the page terms and store them locally
 		if (page.getParseData() instanceof HtmlParseData) { // make sure document has HTML data
