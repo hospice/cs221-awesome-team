3657f1eac99bab739b3d55e03ca64d76467cf107
pensadov@gmail.com <pensadov@gmail.com@9f389937-5a79-6ef3-705e-07bf4cbd8fe1>
2013-01-26 21:46:31 +0000

    Fixed paths to be relative to project folder

--- trunk/project2/Assignment 2/src/ir/assignments/three/Crawler.java
+++ trunk/project2/Assignment 2/src/ir/assignments/three/Crawler.java
@@ -22,11 +22,11 @@ public class Crawler {
 	 * General code structure borrowed from: http://code.google.com/p/crawler4j/source/browse/src/test/java/edu/uci/ics/crawler4j/examples/basic/BasicCrawlController.java 
 	 */
 	public static Collection<String> crawl(String seedURL) {
-		return crawl(seedURL, new DocumentStorage("/docStorage"));		
+		return crawl(seedURL, new DocumentStorage("docStorage"));		
 	}
 	
 	public static Collection<String> crawl(String seedURL, IDocumentStorage docStorage) {
-		return crawl(seedURL, "/intermediateStorage", docStorage, 2, 10); //TODO: change to -1 in final version (unlimited depth and docs)
+		return crawl(seedURL, "intermediateStorage", docStorage, 2, 10); //TODO: change to -1 in final version (unlimited depth and docs)
 	}
 	
 	public static Collection<String> crawl(String seedURL, String intermediateStoragePath, IDocumentStorage documentStorage, int maxDepth, int maxPages) {
