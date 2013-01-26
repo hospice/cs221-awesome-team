4b609637f2b5369a1a8867c8aac5a3c6fbd7e45d
pensadov@gmail.com <pensadov@gmail.com@9f389937-5a79-6ef3-705e-07bf4cbd8fe1>
2013-01-26 21:39:53 +0000

    Made document storage thread-safe (to allow multiple crawler threads to add to it at the same time)

--- trunk/project2/Assignment 2/src/ir/assignments/three/DocumentStorage.java
+++ trunk/project2/Assignment 2/src/ir/assignments/three/DocumentStorage.java
@@ -12,61 +12,72 @@ import java.util.UUID;
 public class DocumentStorage implements IDocumentStorage {
 	private String storagePath;
 	private String lineSeparator = System.getProperty("line.separator");
-	
+	private static final String UNIQUE_ID = "e6ff0549-d27b-4ada-adaf-71e671b0a93f";
+
 	public DocumentStorage(String storagePath) {
 		this.storagePath = storagePath;
 	}
 
 	public void storeDocument(String url, String text) {
-		// Make sure storage path exists
-		File storageDir = new File(this.storagePath);
-		storageDir.mkdir();
+		synchronized (getUrlLock(url)) {
+			// Make sure storage path exists
+			File storageDir = new File(this.storagePath);
+			storageDir.mkdir();
 
-		try {
-			// Try to find an existing local copy of the URL
-			File urlFile = getDocumentFromUrl(storageDir, url);
-			
-			// If no match found, create a new file (URL hash value + random string)
-			if (urlFile == null) {
-				urlFile = new File(storageDir, getURLHash(url) + UUID.randomUUID().toString() + ".txt");
+			try {
+				// Try to find an existing local copy of the URL
+				File urlFile = getDocumentFromUrl(storageDir, url);
+
+				// If no match found, create a new file (URL hash value + random string)
+				if (urlFile == null) {
+					urlFile = new File(storageDir, getUrlHash(url) + UUID.randomUUID().toString() + ".txt");
+				}
+
+				// Write to the file
+				writeDocument(urlFile, url, text);
 			}
-			
-			// Write to the file
-			writeDocument(urlFile, url, text);
-		}
-		catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-			e.printStackTrace();
+			catch (IOException e) {
+				System.out.println("Error: " + e.getMessage());
+				e.printStackTrace();
+			}
 		}
 	}
 
 	public String getDocument(String url) {
-		// Make sure storage path exists
-		File storageDir = new File(this.storagePath);
-		if (!storageDir.exists())
+		synchronized (getUrlLock(url)) {
+			// Make sure storage path exists
+			File storageDir = new File(this.storagePath);
+			if (!storageDir.exists())
+				return null;
+
+			// Find the URL file by 1) hash in the filename and 2) URL in the contents
+			try {
+				File urlDoc = getDocumentFromUrl(storageDir, url);
+				if (urlDoc != null)
+					return readDocument(urlDoc);
+			}
+			catch (FileNotFoundException e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+			catch (IOException e) {
+				System.out.println("Error: " + e.getMessage());
+			}
+
+			// Document not found
 			return null;
-
-		// Find the URL file by 1) hash in the filename and 2) URL in the contents
-		try {
-			File urlDoc = getDocumentFromUrl(storageDir, url);
-			if (urlDoc != null)
-				return readDocument(urlDoc);
 		}
-		catch (FileNotFoundException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-		catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-		
-		// Document not found
-		return null;
-	}
-
-	private String getURLHash(String url) {
-		return Math.abs(url.hashCode()) + "";
 	}
 	
+	private Object getUrlLock(String url) {
+		// Lock based on the global instance of a string (unique_id + URL hash value)
+		// The unique id is to coincidences that would lock on unintended strings
+		return (UNIQUE_ID + getUrlHash(url)).intern();
+	}
+
+	private String getUrlHash(String url) {
+		return Math.abs(url.hashCode()) + "";
+	}
+
 	private void writeDocument(File file, String url, String text) throws IOException {
 		FileWriter stream = new FileWriter(file);
 		BufferedWriter writer = new BufferedWriter(stream);
@@ -75,7 +86,7 @@ public class DocumentStorage implements IDocumentStorage {
 		writer.flush();
 		writer.close();
 	}
-	
+
 	private String readDocument(File file) throws IOException, FileNotFoundException {
 		BufferedReader reader = null;
 		try {
@@ -98,7 +109,7 @@ public class DocumentStorage implements IDocumentStorage {
 				reader.close();
 		}
 	}
-	
+
 	private String getUrlFromDocument(File file) throws IOException {
 		BufferedReader reader = null;
 		try {
@@ -110,22 +121,22 @@ public class DocumentStorage implements IDocumentStorage {
 				reader.close();
 		}
 	}
-	
+
 	private File getDocumentFromUrl(File directory, String url) throws IOException {
 		File urlDoc = null;
-		String urlHash = getURLHash(url);
-		
+		String urlHash = getUrlHash(url);
+
 		for (File possibleFile : directory.listFiles()) {
 			if (!possibleFile.getName().startsWith(urlHash))
 				continue;
-			
+
 			// Check the first line of the doc (the URL)
 			if (url.equals(getUrlFromDocument(possibleFile))) {
 				urlDoc = possibleFile; // match found
 				break;
 			}
 		}
-		
+
 		return urlDoc;
 	}
 }
