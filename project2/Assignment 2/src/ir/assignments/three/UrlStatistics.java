02ed36b420b70ae1d435b07cc0dbe990fda9b577
vibhore20002000 <vibhore20002000@gmail.com>
2013-01-28 21:44:15 +0000

    Wrote counting of unique pages method.

--- project2/Assignment 2/src/ir/assignments/three/UrlStatistics.java
+++ project2/Assignment 2/src/ir/assignments/three/UrlStatistics.java
@@ -3,30 +3,49 @@ package ir.assignments.three;
 import ir.assignments.two.a.*;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 
 public class UrlStatistics {
 	public static int countUniquePages(Collection<String> urls) {
 		// Count the number of unique pages in the URL list (e.g. http://www.example.com/test.php and http://www.example.com/test.php?param are 2 URLs but 1 page
-		return -1;
+		HashSet<String> hm = new HashSet<String>();
+		Iterator<String> iterator;
+		int numbersOfUniquePages ;
+
+		iterator = urls.iterator();     
+		while (iterator.hasNext()){
+			String a = (String) iterator.next();
+			if(a.contains("?")){
+				int i=a.indexOf("?");
+				a = a.substring(0, i);
+			}
+			System.out.println(a);
+			hm.add(a);
+		}
+		numbersOfUniquePages = hm.size();
+		
+		return numbersOfUniquePages ;  
+
 	}
-	
+
 	public static List<Frequency> countSubdomains(Collection<String> urls) {
 		// http://vision.ics.uci.edu, 10
-        // etc.
+		// etc.
 		return null;
 	}
-	
+
 	public static String getLongestPage(Collection<String> urls, IDocumentStorage docStorage) {
 		// Get the page with the most terms (including or excluding stop words?)
 		return null;
 	}
-	
+
 	public static List<String> getMostCommonWords(Collection<String> urls, IDocumentStorage docStorage) {
 		// Get top 500 most common words across all documents (excluding stop words)
 		return null;
 	}
-	
+
 	public static List<String> getMostCommon2Grams(Collection<String> urls, IDocumentStorage docStorage) {
 		// Get top 20 2-grams excluding stop words
 		return null;
