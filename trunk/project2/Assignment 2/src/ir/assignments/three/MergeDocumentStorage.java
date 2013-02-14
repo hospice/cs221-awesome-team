197f836e8228d52e651321b66d17df75ed37e898
pensadov@gmail.com <pensadov@gmail.com@9f389937-5a79-6ef3-705e-07bf4cbd8fe1>
2013-02-14 01:07:43 +0000

    size doesn't work for jdbm2

--- trunk/project2/Assignment 2/src/ir/assignments/three/MergeDocumentStorage.java
+++ trunk/project2/Assignment 2/src/ir/assignments/three/MergeDocumentStorage.java
@@ -9,21 +9,17 @@ public class MergeDocumentStorage {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		DocumentStorage documentStorage1 = new DocumentStorage("docStorage\\docStorage1");
+		DocumentStorage documentStorage1 = new DocumentStorage("docStorage\\docStorage1"); //put smaller one here!
 		DocumentStorage documentStorage2 = new DocumentStorage("docStorage\\docStorage2");
 		
-		try {
-			boolean firstIsSmaller = documentStorage1.getSize() < documentStorage2.getSize();
-			DocumentStorage mergeFrom = firstIsSmaller ? documentStorage1 : documentStorage2;
-			DocumentStorage mergeTo = firstIsSmaller ? documentStorage2 : documentStorage1;
-			
+		try {			
 			int i = 0;
-			for (HtmlDocument doc : mergeFrom.getAll()) {
+			for (HtmlDocument doc : documentStorage1.getAll()) {
 				if (i % 500 == 0)
 					System.out.println(i + "");
 				
 				i++;				
-				mergeTo.storeDocument(doc.getUrl(), doc.getHtml());				
+				documentStorage2.storeDocument(doc.getUrl(), doc.getHtml());				
 			}
 		}
 		finally {
