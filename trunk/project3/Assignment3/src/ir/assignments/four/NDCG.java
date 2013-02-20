79c5caafee38c41655dba424a2d5a472f914f4d6
pensadov@gmail.com <pensadov@gmail.com@9f389937-5a79-6ef3-705e-07bf4cbd8fe1>
2013-02-20 03:22:19 +0000

    I think this is the correct implementation

--- trunk/project3/Assignment3/src/ir/assignments/four/NDCG.java
+++ trunk/project3/Assignment3/src/ir/assignments/four/NDCG.java
@@ -6,35 +6,25 @@ import java.util.List;
 public class NDCG {
 	
 	public static void main(String[] args) {
-		List<String> urls = Arrays.asList(new String[] { "1", "2", "3", "5", "4" });
+		List<String> urls = Arrays.asList(new String[] { "1", "2", "3", "4", "5" });
 		List<String> oracleUrls = Arrays.asList(new String[] { "1", "2", "3", "4", "5" });
 		
-		System.out.println(NDCG.calculate(urls, oracleUrls, 5));
+		System.out.println(NDCG.getNDCG(urls, oracleUrls, 5));
 	}
 
-	public static double calculate(List<String> urls, List<String> oracleUrls, int r) {
+	public static double getNDCG(List<String> urls, List<String> oracleUrls, int r) {
 		// get DCG of urls
-		double[] urlDCG = getDCG(urls, oracleUrls, r);
+		double urlDCG = getDCG(urls, oracleUrls, r);
 		
 		// get DCG of perfect ranking
-		double[] perfectDCG = getDCG(oracleUrls, oracleUrls, r);
+		double perfectDCG = getDCG(oracleUrls, oracleUrls, r);
 		
 		// normalize by dividing
-		double[] normalized = new double[urlDCG.length];
-		for (int i = 0; i < urlDCG.length; i++) {
-			normalized[i] = urlDCG[i] / perfectDCG[i];
-		}
-		
-		// The final score is just the sum of the normalized scores
-		double score = 0;
-		for (int i = 0; i < normalized.length; i++) {
-			score += normalized[i];
-		}
-		
-		return score;
+		double normalized = urlDCG / perfectDCG;
+		return normalized;
 	}
 	
-	private static double[] getDCG(List<String> urls, List<String> oracleUrls, int p) {
+	private static double getDCG(List<String> urls, List<String> oracleUrls, int p) {
 		double[] scores = new double[p];
 		
 		for (int i = 0; i < p; i++) {
@@ -42,11 +32,18 @@ public class NDCG {
 			
 			if (i > 0) {
 				// for all positions after the first one, reduce the "gain"
+				double logValue = (Math.log(i + 1) / Math.log(2));
 				scores[i] /= (Math.log(i + 1) / Math.log(2)); // log base 2
 			}
 		}
 		
-		return scores;
+		// Score is the sum
+		double score = 0;
+		for (int i = 0; i < scores.length; i++) {
+			score += scores[i];
+		}
+		
+		return score;
 	}
 	
 	private static double getRelevance(String url, List<String> oracleUrls) {
