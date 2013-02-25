6b069bfcdd892d9bd74edc0282be246336743f5c
pensadov <pensadov@gmail.com>
2013-02-25 07:59:22 +0000

    Added local caching to google searches

--- project3/Assignment3/src/ir/assignments/four/GoogleSearcher.java
+++ project3/Assignment3/src/ir/assignments/four/GoogleSearcher.java
@@ -1,10 +1,14 @@
 package ir.assignments.four;
 
+import ir.assignments.three.helpers.FileHelper;
+
+import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.jsoup.Jsoup;
@@ -17,15 +21,20 @@ public class GoogleSearcher {
 	public static void main(String[] args) {
 		// For testing purposes
 		GoogleSearcher searcher = new GoogleSearcher();
-		List<String> results = searcher.getSearchResults("machine learning", 10);
+		List<String> results = searcher.getSearchResults("machine learning", 5);
 		for (String url : results) {
 			System.out.println(url);
 		}
 	}
 
-	private static final String GOOGLE_SEARCH_URL = "https://www.google.com/search?q=[QUERY]&hl=en&start=[START]&btnG=Google+Search&gbv=1";
+	private static final String GOOGLE_SEARCH_URL = "https://www.google.com/search?q=[QUERY]&hl=en&start=0&btnG=Google+Search&gbv=1";
 
-	public List<String> getSearchResults(String query, int topN) {
+	public List<String> getSearchResults(String query, int count) {
+		
+		// First check if results cached locally (prevent requesting Google searches too much)
+		String[] cachedResults = getResultsFromCache(query);
+		if (cachedResults != null)
+			return Arrays.asList(cachedResults);
 
 		// Get the search url
 		String encodedQuery = getQueryString(query);
@@ -33,42 +42,30 @@ public class GoogleSearcher {
 
 		// Do search until we have enough results
 		ArrayList<String> urlResults = new ArrayList<String>();
-		int start = 0;
-		while (urlResults.size() < topN) {
-			String pageSearchUrl = searchUrl.replace("[START]", start + "");
+		try {
+			// Download the page using jsoup
+			// (use Firefox's user agent string so google doesn't block request)
+			Document pageDoc = Jsoup.connect(searchUrl).userAgent("Mozilla/6.0 (Windows NT 6.2; WOW64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1").get();
 
-			try {
-				// Download the page using jsoup
-				// (user Firefox's user agent string so google doesn't block request)
-				Document pageDoc = Jsoup.connect(pageSearchUrl).userAgent("Mozilla/6.0 (Windows NT 6.2; WOW64; rv:16.0.1) Gecko/20121011 Firefox/16.0.1").get();
+			// Get the URLs of the search results
+			Elements searchResults = pageDoc.select("li[class=g] a");
+			for (Element result : searchResults) {
+				if (result.text().equals("Cached") || result.text().equals("Similar")) // don't include cached or related links
+					continue;
 
-				// Get the URLs of the search results
-				Elements searchResults = pageDoc.select("li[class=g] a");
-				for (Element result : searchResults) {
-					if (result.text().equals("Cached") || result.text().equals("Similar")) // don't include cached or related links
-						continue;
-					
-					String href = result.attr("href");
-					String extractedUrl = extractUrl(href);
-					urlResults.add(extractedUrl);
-				}
+				String href = result.attr("href");
+				String extractedUrl = extractUrl(href);
+				urlResults.add(extractedUrl);
+				if (urlResults.size() >= count)
+					break;
 			}
-			catch (IOException e) {
-				e.printStackTrace();
-				break;
-			}
-
-			// Wait a second before requesting more results to be polite to google
-			try {
-				Thread.sleep(1000);
-			}
-			catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-
-			// Get 10 next results if necessary
-			start += 10;
 		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		// Cache results for future calls
+		addResultsToCache(query, urlResults.toArray(new String[urlResults.size()]));
 
 		return urlResults;
 	}
@@ -101,5 +98,42 @@ public class GoogleSearcher {
 		catch (UnsupportedEncodingException e) {
 			return "";
 		}
+	}
+	
+	private static final String googleQueryCacheFolder = "googleQueryCache";
+
+	private void addResultsToCache(String query, String[] results) {
+		// Save the query results to a file		
+		String cacheFilename = getCacheFilename(query);
+		File cacheFile = new File(cacheFilename);
+
+		try {
+			new File(googleQueryCacheFolder).mkdir();
+			FileHelper.writeFile(cacheFile, results);
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private String[] getResultsFromCache(String query) {
+		String cacheFilename = getCacheFilename(query);
+		File cacheFile = new File(cacheFilename);
+
+		try {
+			if (cacheFile.exists()) {
+				String results = FileHelper.readFile(cacheFile, 0);
+				return results.split(FileHelper.lineSeparator);
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
+	private String getCacheFilename(String query) {
+		return googleQueryCacheFolder + "\\" + Math.abs(query.hashCode()) + ".txt";
 	}
 }
