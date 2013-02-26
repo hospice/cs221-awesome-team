d35397b4b551e5464893541c5cd71904b0d27811
pensadov <pensadov@gmail.com>
2013-02-26 19:58:22 +0000

    Added progress

--- project3/Assignment3/src/ir/assignments/four/checkDocStorage.java
+++ project3/Assignment3/src/ir/assignments/four/checkDocStorage.java
@@ -6,11 +6,22 @@ import ir.assignments.three.storage.IDocumentStorage;
 
 public class checkDocStorage {
 
+	public static void main(String[] args) {
+		System.out.println(check("http://www.ics.uci.edu/prospective/en/degrees/software-engineering/").toString());
+	}
+	
 	public static Boolean check(String Oracleurl){
+		
 		DocumentStorage docStorage = new DocumentStorage("docStorage\\docStorage");
 
+		int i = 0;
 		for (String url : docStorage.getCrawledUrls()) {
-			// Display number of processed documents to keep track of progress				
+			// Display number of processed documents to keep track of progress
+			if (i % 500 == 0) {
+				System.out.println(i + "");
+			}
+			i++;
+			
 			if(url.equals(Oracleurl)){
 				return true;
 			}
