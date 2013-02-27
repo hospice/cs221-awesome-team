f7be6f330de04dfbdd13b51edb658a44822ddbc6
pensadov <pensadov@gmail.com>
2013-02-27 01:04:33 +0000

    Quickly check if url was crawled

--- project3/Assignment3/src/ir/assignments/four/checkDocStorage.java
+++ project3/Assignment3/src/ir/assignments/four/checkDocStorage.java
@@ -1,31 +1,16 @@
 package ir.assignments.four;
 
-import ir.assignments.three.helpers.URLHelper;
 import ir.assignments.three.storage.DocumentStorage;
-import ir.assignments.three.storage.IDocumentStorage;
 
 public class checkDocStorage {
 
 	public static void main(String[] args) {
-		System.out.println(check("http://www.ics.uci.edu/prospective/en/degrees/software-engineering/").toString());
+		
+		System.out.println(check("http://www.ics.uci.edu/~ziv/ooad/intro_to_se/tsld023.htm").toString());
 	}
 	
-	public static Boolean check(String Oracleurl){
-		
+	public static Boolean check(String targetUrl){
 		DocumentStorage docStorage = new DocumentStorage("docStorage\\docStorage");
-
-		int i = 0;
-		for (String url : docStorage.getCrawledUrls()) {
-			// Display number of processed documents to keep track of progress
-			if (i % 500 == 0) {
-				System.out.println(i + "");
-			}
-			i++;
-			
-			if(url.equals(Oracleurl)){
-				return true;
-			}
-		}
-		return false;
+		return docStorage.getDocument(targetUrl) != null;
 	}
 }
