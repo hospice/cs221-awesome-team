e1c7303b3154898e955151ef3132896229e051e9
pensadov <pensadov@gmail.com>
2013-03-02 10:10:49 +0000

    Lazy load, split important body into getHeaders() and getImportantBody(). Added more headers that are detected

--- project2/Assignment 2/src/ir/assignments/three/storage/HtmlDocument.java
+++ project2/Assignment 2/src/ir/assignments/three/storage/HtmlDocument.java
@@ -1,9 +1,9 @@
 package ir.assignments.three.storage;
 
-import java.net.MalformedURLException;
+import ir.assignments.three.helpers.URLHelper;
+
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
 import java.util.HashMap;
 
 import org.jsoup.Jsoup;
@@ -13,16 +13,25 @@ import org.jsoup.select.Elements;
 
 public class HtmlDocument {
 	private final static String lineSeparator = System.getProperty("line.separator");
+	private boolean hasLoaded;
 	private Document parsedDoc;
 	private String url;
 	private String html;
 
 	public HtmlDocument(String url, String html) {
 		// Parse document using jsoup (http://jsoup.org/)
-		this.parsedDoc = Jsoup.parse(html);
 		this.url = url;
 		this.html = html;
+		this.hasLoaded = false;
+	}
 
+	private void load() {
+		if (this.hasLoaded)
+			return;
+
+		this.hasLoaded = true;
+		this.parsedDoc = Jsoup.parse(html);
+		
 		// Follow meta tag redirects
 		try {
 			Elements meta = this.parsedDoc.select("html head meta");
@@ -50,16 +59,23 @@ public class HtmlDocument {
 	public String getUrl() {
 		return this.url;
 	}
+	
+	public String getUrlDomain() {
+		return URLHelper.getDomain(getUrl());
+	}
 
 	public String getHtml() {
+		this.load();
 		return this.html;
 	}
 
 	public String getTitle() {
+		this.load();
 		return this.parsedDoc.title();
 	}
 
 	public String getDescription() {
+		this.load();
 		String description = "";
 
 		// Get description from meta tag(s) (e.g. <meta name="description" content="This is the description" />)
@@ -74,15 +90,32 @@ public class HtmlDocument {
 	}
 
 	public String getBody() {
+		this.load();
 		if (this.parsedDoc.body() != null)
 			return this.parsedDoc.body().text();
 		return "";
 	}
-
-	public String getImportantBody() {
+	
+	public String getHeaders() {
+		this.load();
 		StringBuilder text = new StringBuilder();
 
-		Elements elements = this.parsedDoc.select("h1,h2,b,strong");
+		Elements elements = this.parsedDoc.select("h1,h2,div[id*=title],div[class*=title],span[id*=title],span[class*=title]");
+		if (elements != null) {
+			for (Element element : elements) {
+				text.append(element.text());
+				text.append(lineSeparator);
+			}
+		}
+
+		return text.toString();
+	}
+
+	public String getImportantBody() {
+		this.load();
+		StringBuilder text = new StringBuilder();
+
+		Elements elements = this.parsedDoc.select("b,strong,em");
 		if (elements != null) {
 			for (Element element : elements) {
 				text.append(element.text());
@@ -98,6 +131,7 @@ public class HtmlDocument {
 	}
 
 	public HashMap<String, String> getOutgoingLinks() {
+		this.load();
 		HashMap<String, String> outgoingLinks = new HashMap<String, String>(); // (URL, anchor text)
 
 		Elements links = this.parsedDoc.select("a");
@@ -106,41 +140,41 @@ public class HtmlDocument {
 				String href = link.attr("href");
 				if (href.contains("javascript:") || href.contains("mailto:") || href.contains("https"))
 					continue;
-				
+
 				// Get absolute URL
 				String absoluteHref = combineUrls(this.url, href);
-				
+
 				if (absoluteHref == null)
 					continue;
-				
+
 				// Make sure URL within same domain
 				if (!absoluteHref.contains("ics.uci.edu") || absoluteHref.contains("https"))
 					continue;
-				
+
 				// Append to the anchor text
 				String currentAnchorText = outgoingLinks.get(absoluteHref);
 				if (currentAnchorText == null)
 					currentAnchorText = "";
-				
+
 				currentAnchorText += " " + link.text();
 				outgoingLinks.put(absoluteHref, currentAnchorText.trim());
 			}
 		}
-		
+
 		return outgoingLinks;
 	}
 
-	private static String combineUrls(String url, String relativeUrl) {
+	private String combineUrls(String url, String relativeUrl) {
 		if (relativeUrl.contains("http"))
 			return relativeUrl;
-		
+
 		try {
 			URI baseUri = new URI(url);
 			return baseUri.resolve(new URI(relativeUrl)).toString();
 		}
 		catch (URISyntaxException e) {
-//			System.out.println("Error on URL: (" + url + ", " + relativeUrl + ")");
-//			e.printStackTrace();
+			//			System.out.println("Error on URL: (" + url + ", " + relativeUrl + ")");
+			//			e.printStackTrace();
 		}
 
 		return null;
