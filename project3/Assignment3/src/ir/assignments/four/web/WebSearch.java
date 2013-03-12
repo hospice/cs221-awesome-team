75c53f8c754b534bd5323598df08b1e8d55c5511
vibhore20002000 <vibhore20002000@gmail.com>
2013-03-12 21:58:01 +0000

    Committing SearchFiles.java, ResultSet.java and WebSearch.java, and adding highlighter library.

--- project3/Assignment3/src/ir/assignments/four/web/WebSearch.java
+++ project3/Assignment3/src/ir/assignments/four/web/WebSearch.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.search.highlight.InvalidTokenOffsetsException;
+
+import ir.assignments.four.HighlighterUtility;
 import ir.assignments.four.ResultSet;
 import ir.assignments.four.SearchFiles;
 import ir.assignments.four.storage.DocumentStorage;
@@ -12,34 +16,53 @@ import ir.assignments.four.storage.HtmlDocument;
 public class WebSearch {
 	private SearchFiles indexSearch;
 	private DocumentStorage docStorage;
-	
+
 	public WebSearch() {
 		try {
-			this.indexSearch = new SearchFiles("docIndexEnhanced");
-			this.docStorage = new DocumentStorage("docStorage\\docStorage");
+			this.indexSearch = new SearchFiles("C:\\Users\\Vibhor Mathur\\Documents\\IR\\Project\\project3\\Assignment3\\docIndexstoring");
+			this.docStorage = new DocumentStorage("C:\\Users\\Vibhor Mathur\\Documents\\IR\\Project\\project3\\Assignment3\\docStorage\\docStorage");
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public WebResultSet search(String query, int page, int maxPerPage) {
 		ResultSet results = this.indexSearch.search(query, page, maxPerPage);
-		
+
 		// Create the results 
 		ArrayList<SearchResult> displayResults = new ArrayList<SearchResult>();
-		for (String url : results.getUrls()) {
+		List<String> urls = results.getUrls();
+
+		for (int i = 0; i < urls.size(); i++) {
+			String url = urls.get(i);
 			String html = docStorage.getDocument(url);
 			HtmlDocument doc = new HtmlDocument(url, html);
-			
+
 			String title = doc.getTitle();
-			String description = doc.getBody();
-			description = description.substring(0, Math.min(100, description.length())).trim() + "...";
-			
+			String description="";
+
+			try {
+				description = this.indexSearch.getHighlights(url, query, results.getIds().get(i), doc.getBody());
+			}
+			catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			catch (InvalidTokenOffsetsException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+
 			SearchResult result = new SearchResult(title, url, description);
 			displayResults.add(result);
 		}
-		
+
 		SearchResult[] displayResultsArray = displayResults.toArray(new SearchResult[displayResults.size()]);
 		return new WebResultSet(displayResultsArray, results.getTotalHits(), page); 
 	}
@@ -48,7 +71,7 @@ public class WebSearch {
 		if (this.indexSearch != null) {
 			this.indexSearch.close();
 		}
-		
+
 		if (this.docStorage != null) {
 			this.docStorage.close();
 		}
