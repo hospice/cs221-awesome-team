4d4840207c4bf1cd8f74b8aee21e1a4c399dbf09
pensadov@gmail.com <pensadov@gmail.com@9f389937-5a79-6ef3-705e-07bf4cbd8fe1>
2013-03-13 04:34:13 +0000

    Bug fix: Don't cut off description in middle of <b></b>

--- trunk/project3/Assignment3/src/ir/assignments/four/web/WebSearch.java
+++ trunk/project3/Assignment3/src/ir/assignments/four/web/WebSearch.java
@@ -43,8 +43,21 @@ public class WebSearch {
 			try {
 				title = this.indexSearch.getHighlights("title", query, doc.getTitle(), true); // entire "document"
 				description = this.indexSearch.getHighlights("content", query, doc.getBody(), false); // get fragments
-				if (description.length() > 200)
-					description = description.substring(0, 200);
+				if (description.length() > 250) {
+					// Too long, shorten
+					int cutoffIndex = 250;
+					
+					// Make sure not cutting off a bold tag (or it'll make the rest of the page bold)
+					String lowerCaseDescription = description.toLowerCase();
+					int lastBoldIndex = lowerCaseDescription.lastIndexOf("<b>");
+					if (lastBoldIndex > -1) {
+						int lastBoldClosingIndex = lowerCaseDescription.indexOf("</b>", lastBoldIndex);
+						if (lastBoldClosingIndex + "</b>".length() > cutoffIndex)
+							cutoffIndex = lastBoldClosingIndex + "</b>".length();
+					}
+					
+					description = description.substring(0, cutoffIndex) + "...";					
+				}
 			}
 			catch (Exception e) {
 				e.printStackTrace();
