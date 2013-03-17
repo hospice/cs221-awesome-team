8f10132ea09309ba50d28f726a315cc2df3dfd0b
pensadov <pensadov@gmail.com>
2013-03-17 20:18:00 +0000

    Added keyboard support for autocomplete

--- project3/Assignment3/WebContent/scripts/autocomplete.js
+++ project3/Assignment3/WebContent/scripts/autocomplete.js
@@ -1,17 +1,23 @@
 var lastQuery = "";
+var canUpdateAutoSuggest = true; 
 
 $(document).ready(function() {
 	lastQuery = $("#txtQuery").val(); // don't show autocomplete when page loads
 	
 	setInterval(function() {
+		if (!canUpdateAutoSuggest)
+			return;
+		
+		// Only submit request to server if query has changed
 		var currentQuery = $("#txtQuery").val();
 		if (lastQuery !== currentQuery) {
 			lastQuery = currentQuery;
-			if (currentQuery === "") {
+			if (currentQuery === "") { // hide autosuggest if no query entered
 				hideSuggest();
 				return;
 			}
 			
+			// Request autocomplete list from server
 			$.ajax({
 				url : "services/autocomplete-service.jsp",
 				data : {
@@ -34,6 +40,7 @@ $(document).ready(function() {
 		}
 	}, 500);
 	
+	// Hide autocomplete box if user clicks outside of the textbox or autocomplete dropdown
 	$(document).click(function(e) {
 		// Check if mouse click was on search textbox or on the autosuggest dropdown
 		var inSearchElements = $(e.target).is("#txtQuery") || $(e.target).is("#autocomplete") || $(e.target).is("#autocomplete *");
@@ -43,7 +50,56 @@ $(document).ready(function() {
 			hideSuggest();
 		}
 	});
+	
+	// Allow user to use up and down arrow keys to navigate autocomplete suggestions
+	$("#txtQuery").keyup(function(e) {
+		var keyCode = e.keyCode || e.which;
+		var currentSelected = $("#autocomplete .item.hover");
+		var nextToSelect = null;
+		
+		if (keyCode === 40) { // Down arrow key
+			if (currentSelected.length > 0) {
+				// Find the next item in the list
+				nextToSelect = currentSelected.next(".item");
+			}
+			else {
+				// Select first one
+				nextToSelect = $("#autocomplete .item:first");
+			}
+		}
+		else if (keyCode === 38) { // Up arrow key
+			if (currentSelected.length > 0) {
+				// Find the previous item in the list
+				nextToSelect = currentSelected.prev(".item");
+			}
+			else {
+				// Select last one
+				nextToSelect = $("#autocomplete .item:last");
+			}
+		}
+		
+		// Update highlight
+		if (nextToSelect !== null && nextToSelect.length > 0) {
+			highlight(nextToSelect);
+			
+			// Set text in query textbox to highlighted text
+			var highlightedText = nextToSelect.text();
+			canUpdateAutoSuggest = false; // don't refresh autosuggest (otherwise list will change while user navigates through it)			
+			$("#txtQuery").val(highlightedText);
+			lastQuery = highlightedText;
+			canUpdateAutoSuggest = true; // update if user types anything new
+		}
+	});
 });
+
+function highlight(item) {
+	$("#autocomplete .item").removeClass("hover"); // remove highlight from everything else
+	$(item).addClass("hover");
+}
+
+function unhighlight(item) {
+	$(item).removeClass("hover");
+}
 
 function updateAndShowSuggest(itemsHtml) {
 	var txtQuery = $("#txtQuery");
@@ -63,6 +119,13 @@ function updateAndShowSuggest(itemsHtml) {
 		
 		// Submit the form (initiate search)
 		$("#frmSearch").submit();
+	});
+	
+	// Highlight events for mouse
+	$("#autocomplete .item, #autocomplete .item *").mouseenter(function() {
+		highlight($(this).closest(".item"));
+	}).mouseout(function() {
+		unhighlight($(this).closest(".item"));
 	});
 }
 
